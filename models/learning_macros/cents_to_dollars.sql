95642d92773928b454615cd380a5f81f5ee7c5c2
Jachwa Dolne <jachwadolne@jachwas-mbp.mynetworksettings.com>
2026-06-03 10:45:19 -0400

    files to commit

--- models/learning_macros/cents_to_dollars.sql
+++ models/learning_macros/cents_to_dollars.sql
@@ -3,6 +3,13 @@ Let's pretend the column amount in the raw_payments tables is in cents not dolla
 This table used a macro to change this information from cents to dollars.
 #}
 
+{{ config (
+    {
+         "schema":"staging" 
+    }
+)}}
+-- If you want the model to land in just staging and not dbt_jdolne_staging, you need to watch 
+-- https://learn.getdbt.com/learn/course/jinja-macros-and-packages-vs-code/advanced-jinja-and-macros/advanced-jinja-and-macros?page=5
 with payments as (
     SELECT *
     FROM {{ ref('raw_payments') }}
